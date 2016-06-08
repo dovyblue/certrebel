ba9dbbd09a6609ed033e82e6976465f56c101fc7
Rene Midouin <renemidouin@gmail.com>
2016-06-07 21:40:19 -0400

    added course grid search

--- forms/search/redirect.php
+++ forms/search/redirect.php
@@ -1,10 +1,14 @@
 <?php
-$search = htmlentities($_POST['search']);
-$search = str_replace(" ", "+", $search);
 $server = $_SERVER['SERVER_NAME'];
-if ($search != "")
-	header("location: http://$server/courses/search/$search");
-else
-	header("location: http://$server/courses");
+$search = htmlentities($_POST['search']);
+$search_category = htmlentities($_POST['search_category']);
+$search_location = htmlentities($_POST['search_location']);
+$search_category = empty($search_category) ? "all" : strtolower($search_category);
+$search_location = empty($search_location) ? "all" : strtolower($search_location);
+$search = str_replace(" ", "+", $search);
 
+if ($search != "")
+	header("location: http://$server/courses/category=$search_category/location=$search_location/search=$search");
+else 
+	header("location: http://$server/courses/category=$search_category/location=$search_location");
 ?>
--- functions.php
+++ functions.php
@@ -4,6 +4,15 @@ $dbname 	 = 'CertRebel';
 $dbuser 	 = 'root'; 
 $dbpass 	 = '$lice0fBread@123'; 
 
+function limit_text($text, $limit) {
+	if (str_word_count($text, 0) > $limit) {
+			$words = str_word_count($text, 2);
+			$pos = array_keys($words);
+			$text = substr($text, 0, $pos[$limit]) . '...';
+	}
+  return $text;
+}
+
 function course_extra_details() {
 	$url = __DIR__.'/json_files/course_extra_details.json';
 	if (!file_exists($url) || !filesize($url))
@@ -37,6 +46,30 @@ function course_info() {
 
 	if (!file_exists($url) || !filesize($url)) 
 		$url = 'http://' . $_SERVER['HTTP_HOST'].'/scripts/php/course_info.php';
+	
+	$file = file_get_contents($url);
+	$json_a = json_decode($file, true);
+
+	return $json_a;
+}
+
+function course_locations() {
+	$url = __DIR__.'/json_files/course_locations.json';
+
+	if (!file_exists($url) || !filesize($url)) 
+		$url = 'http://' . $_SERVER['HTTP_HOST'].'/scripts/php/course_locations.php';
+	
+	$file = file_get_contents($url);
+	$json_a = json_decode($file, true);
+
+	return $json_a;
+}
+
+function course_categories() {
+	$url = __DIR__.'/json_files/course_categories.json';
+
+	if (!file_exists($url) || !filesize($url)) 
+		$url = 'http://' . $_SERVER['HTTP_HOST'].'/scripts/php/course_categories.php';
 	
 	$file = file_get_contents($url);
 	$json_a = json_decode($file, true);
