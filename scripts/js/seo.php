a5f4fe2a78bf54f42fd29bb1c1385e30d86de1bf
Rene Midouin <renemidouin@gmail.com>
2016-07-18 19:58:31 -0400

    added new google conversion

--- scripts/js/seo.php
+++ scripts/js/seo.php
@@ -16,6 +16,24 @@
 ///* ]]> */
 //</script>
 //
+//<!-- Google Code for Purchased Class From Website Conversion Page -->
+//<script type="text/javascript">
+///* <![CDATA[ */
+//var google_conversion_id = 934308555;
+//var google_conversion_language = "en";
+//var google_conversion_format = "3";
+//var google_conversion_color = "ffffff";
+//var google_conversion_label = "EWKQCJqFvWgQy9XBvQM";
+//var google_remarketing_only = false;
+///* ]]> */
+//</script>
+//<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+//</script>
+//<noscript>
+//<div style="display:inline;">
+//<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/934308555/?label=EWKQCJqFvWgQy9XBvQM&amp;guid=ON&amp;script=0"/>
+//</div>
+//</noscript>
 //
 //<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
 //</script>
